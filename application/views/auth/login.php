aa2c464b577d98962822f06d8628f0e0a8ac8112
Gawat Budi Triprayogo <gawatbudiprayogo2000@gmail.com>
2025-02-24 11:17:18 +0700

    first commit

---
+++ application/views/auth/login.php
@@ -0,0 +1,45 @@
+
+
+    <div class="main-wrapper">
+        <div class="account-content">
+            <div class="login-wrapper">
+                <div class="login-content">
+                    <div class="login-userset">
+                        <div class="login-logo">
+                            <img src="assets/img/logo.png" alt="img">
+                        </div>
+                        <div class="login-userheading">
+                            <h3>Login</h3>
+                            <h4>Please login to your account</h4>
+                        </div>
+                        <div class="form-login">
+                            <label>Email</label>
+                            <div class="form-addons">
+                                <input type="text" id="email" name="email" placeholder="Enter your email address">
+                                <img src="assets/img/icons/mail.svg" alt="img">
+                            </div>
+                        </div>
+                        <div class="form-login">
+                            <label>Password</label>
+                            <div class="pass-group">
+                                <input type="password" id="password" name="password"class="pass-input" placeholder="Enter your password">
+                                <span class="fas toggle-password fa-eye-slash"></span>
+                            </div>
+                        </div>
+                        <div class="form-login">
+                            <div class="alreadyuser">
+                                <h4><a href="forgetpassword.html" class="hover-a">Forgot Password?</a></h4>
+                            </div>
+                        </div>
+                        <div class="form-login">
+                            <button type="submit" a class="btn btn-login" href="index.html">Sign In</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="login-img">
+                    <img src="assets/img/login.jpg" alt="img">
+                </div>
+            </div>
+        </div>
+    </div>
+
