4943c00c64b5713ff98d7bfd68d11f7b784c6e6e
Gawat Budi Triprayogo <gawatbudiprayogo2000@gmail.com>
2025-02-26 10:20:44 +0700

    commit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -4,10 +4,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Auth extends CI_Controller 
 
 {
-    public function index()
+    public function construct()
     {
+        parent::__construct();
+        $this->load->model('User_model');
+    }
+
+    public function index()
+    { 
         $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
+
+    function login()
+    {
+
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $cekuser = $this->Login_model->cekUser('name', 'status', 'aktif', $email); 
+    }
+
+    function logout(){
+        $this->session->sess_destroy();
+        redirect('auth', 'refresh');
+    }
+   
 }
---
+++ application/views/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="XUA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="<?= base_url('assets'); ?>/style.css">
+    </head>
+    <body>
+        <div class="header center">
+            <h1>Welcome page home</h1>
+        </div>
+    </body>
